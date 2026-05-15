e745856ded49dcf5ebb9849c82b6b8c0b6c5c375
Davi Mendes Ramos <davimendesramos123@gmail.com>
2026-05-15 16:15:20 -0300

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import Sobre from "./pages/Sobre/sobre.jsx";
 import Horarios from "./pages/Horarios/horarios.jsx";
+
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/Resendense" element={<Home />} />
-        <Route path="/Resendense/sobre" element={<Sobre />} />
-        <Route path="/Resendense/horarios" element={<Horarios />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/horarios" element={<Horarios />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
+
 export default App;
