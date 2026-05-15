c173fd6fd859c8a3a6bf493616975653cc67a1b3
Davi Mendes Ramos <128087384+DaviMendesRamos@users.noreply.github.com>
2026-05-15 15:44:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="Resendense/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/horarios" element={<Horarios />} />
       </Routes>
